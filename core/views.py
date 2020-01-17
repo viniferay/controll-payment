fb1f8aa3682708b24e09bcdb9a60cb24374c00b0
Vinicius Ferreira <vferreira@mastertech.tech>
2020-01-17 16:59:25 -0300

    Create register method

--- core/views.py
+++ core/views.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.hashers import make_password
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from core.forms import RegisterForm
+
+
+def register(request):
+    form = RegisterForm(request.POST or None)
+    args = {'form': form}
+
+    if form.is_valid():
+        user = form.save()
+        user.password = make_password(form.cleaned_data.get('password'))
+        user.save()
+        args['return'] = 'Usuários cadastrado com sucesso'
+        return render(request, 'register.html', args)
+
+    return render(request, 'register.html', args)
