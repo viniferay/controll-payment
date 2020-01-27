1aac71870e1359b049072315a43cf256db07ca63
Vinicius Ferreira <vferreira@mastertech.tech>
2020-01-27 10:01:16 -0300

    Fix Sett

--- controll_payment/settings.py
+++ controll_payment/settings.py
@@ -129,7 +129,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/staticfiles/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
