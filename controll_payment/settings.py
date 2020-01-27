57bfacf729047c5d1cc9bc0fe142d14fbafd3617
Vinicius Ferreira <vferreira@mastertech.tech>
2020-01-27 09:39:56 -0300

    Fix settings

--- controll_payment/settings.py
+++ controll_payment/settings.py
@@ -124,8 +124,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = os.path.join(BASE_DIR, 'static')
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
